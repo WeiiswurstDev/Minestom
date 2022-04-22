395adc98ffa26d846eaf3524f5e59d9aaf496ca5
TheMode <themode@outlook.fr>
2022-04-22 02:19:48 +0200

    More tests
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/test/java/net/minestom/server/tag/TagDatabaseTest.java
+++ src/test/java/net/minestom/server/tag/TagDatabaseTest.java
@@ -3,6 +3,7 @@ package net.minestom.server.tag;
 import org.jglrxavpok.hephaistos.nbt.NBT;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,6 +18,57 @@ public class TagDatabaseTest {
                 .filter(TagDatabase.Filter.eq(Tag.String("key"), "value")).build();
         var result = db.find(query);
         assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findFilterEq() {
+        TagDatabase db = createDB();
+        var tag = Tag.String("key");
+        var compound1 = NBT.Compound(Map.of("key", NBT.String("value"),
+                "other", NBT.String("otherValue")));
+        var compound2 = NBT.Compound(Map.of("key", NBT.String("value2"),
+                "other", NBT.String("otherValue")));
+
+        db.updateSingle(tag, "value", TagHandler.fromCompound(compound1));
+        db.updateSingle(tag, "value2", TagHandler.fromCompound(compound2));
+
+        var query = TagDatabase.Query.builder()
+                .filter(TagDatabase.Filter.eq(tag, "value")).build();
+        assertEquals(List.of(compound1), db.find(query));
+    }
+
+    @Test
+    public void findMultiEq() {
+        TagDatabase db = createDB();
+        var tag = Tag.String("key");
+        var compound1 = NBT.Compound(Map.of("key", NBT.String("value"),
+                "other", NBT.String("otherValue")));
+        var compound2 = NBT.Compound(Map.of("key", NBT.String("value2"),
+                "other", NBT.String("otherValue")));
+
+        db.updateSingle(tag, "value", TagHandler.fromCompound(compound1));
+        db.updateSingle(tag, "value2", TagHandler.fromCompound(compound2));
+
+        var query = TagDatabase.Query.builder()
+                .filter(TagDatabase.Filter.eq(Tag.String("other"), "otherValue")).build();
+        assertEquals(List.of(compound1, compound2), db.find(query));
+    }
+
+    @Test
+    public void findMultiTreeEq() {
+        TagDatabase db = createDB();
+        var tag = Tag.String("key");
+        var compound1 = NBT.Compound(Map.of("key", NBT.String("value"),
+                "path", NBT.Compound(Map.of("other", NBT.String("otherValue")))));
+        var compound2 = NBT.Compound(Map.of("key", NBT.String("value2"),
+                "path", NBT.Compound(Map.of("other", NBT.String("otherValue")))));
+
+        db.updateSingle(tag, "value", TagHandler.fromCompound(compound1));
+        db.updateSingle(tag, "value2", TagHandler.fromCompound(compound2));
+
+        var query = TagDatabase.Query.builder()
+                .filter(TagDatabase.Filter.eq(Tag.String("other").path("path"), "otherValue")).build();
+        assertEquals(List.of(compound1, compound2), db.find(query));
     }
 
     @Test
